6e2581b7ef9661fde98f1cc31687bb4e923b7a8e
Christinealloyd <christinealloyd@icloud.com>
2025-05-19 10:38:06 +0200

    weather api activity added

---
+++ week03/scripts/apiact.js
@@ -0,0 +1,28 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.749636891882034&lon=6.637300108985603&units=metric&appid=79c0ceae4f9d73b952d59786029584bb';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      throw new Error(await response.text());
+    }
+  } catch (error) {
+    console.error('Caught an error', error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
